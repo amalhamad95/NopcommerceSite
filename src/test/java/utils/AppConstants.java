d48fe98450e7790bb4276e5d64914503b3172c76
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2021-12-29 08:37:16 +0200

    Create AppConstants.java

---
+++ src/test/java/utils/AppConstants.java
@@ -0,0 +1,35 @@
+package utils;
+
+public class AppConstants {
+
+	public static final String BASE_URL = "https://admin-demo.nopcommerce.com/";
+	public static final String ADMIN_BASE_URL = BASE_URL + "Admin/";
+	public static final String LOGIN_URL = BASE_URL + "login";
+
+	public static final String DASHBOARD_URL = BASE_URL + "admin/";
+
+	public static final String PRODUCTS_URL = ADMIN_BASE_URL + "Product/List";
+	public static final String ADD_PRODUCT_URL = ADMIN_BASE_URL + "Product/Create";
+
+	public static final String DISCOUTS_URL = ADMIN_BASE_URL + "Discount/List";
+	public static final String ADD_DISCOUNT_URL = ADMIN_BASE_URL + "Discount/Create";
+	public static final String EDIT_DISCOUNT_URL = ADMIN_BASE_URL + "Discount/Edit";
+
+	public static final String LOGIN_PAGE = "Your store. Login";
+//	public static final String DASHBOARD_PAGE = "Dashboard / nopCommerce administration";
+//	public static final String PRODUCTS_PAGE = "Products / nopCommerce administration";
+//	public static final String ADD_PRODUCT_PAGE = "Add a new product / nopCommerce administration";
+//	public static final String DISCOUNTS_PAGE = "Discounts / nopCommerce administration";
+//	public static final String ADD_DISCOUNT_PAGE = "Add a new discount / nopCommerce administration";
+
+	public static final String DASHBOARD_TITLE = "Dashboard";
+	public static final String PRODUCTS_TITLE = "Products";
+	public static final String ADD_PRODUCT_TITLE = "Add a new product";
+	public static final String DISCOUNTS_TITLE = "Discounts";
+	public static final String ADD_DISCOUNT_TITLE = "Add a new discount";
+	public static final String EDIT_DISCOUNT_TITLE = "Edit discount details";
+
+	public static final String ADD_PRODUCT_POPUP_URL = "Discount/ProductAddPopup?discountId=3&btnId=btnRefreshProducts&formId=discount-form";
+	public static final String ADD_PRODUCT_POPUP_TITLE = "Add a new product";
+
+}
