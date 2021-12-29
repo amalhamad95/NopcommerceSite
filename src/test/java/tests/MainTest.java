6c99a57b4aba2afc90ae9f4c526973a43dff85be
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2021-12-29 10:15:56 +0200

    Update MainTest.java
    Testing Products Page:
    - click on "Catalog" navItem
    - click on "Products" navItem
    - check page url + title
    - create method: assertNavItemSelected(WebElement item)
    - create method: checkTitleAndURL(String url, String title)

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import utils.AppConstants;
@@ -16,6 +20,8 @@ public class MainTest {
 
 		driver = DriversHandler.initGoogleChromeDriver();
 		driver.get(AppConstants.BASE_URL);
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
 		// ------------------- Login Page -------------------
 		Assert.assertTrue(driver.getCurrentUrl().contains(AppConstants.LOGIN_URL));
@@ -37,5 +43,42 @@ public class MainTest {
 		Assert.assertTrue(navDashboard.getAttribute("class").contains("active"));
 		Assert.assertEquals(navDashboard.getCssValue("color"), "rgba(255, 255, 255, 1)");
 
+		// ------------------- Products Page -------------------
+		WebElement navCatalog = driver
+				.findElement(By.xpath("//p[contains(text(), 'Catalog')]/ancestor::a[@href='#']/parent::li"));
+//		WebElement navCatalog = driver.findElements(By.xpath("//p[contains(text(), \"Catalog\")]")).get(0);
+		navCatalog.click();
+		Thread.sleep(1 * 1000);
+		System.out.println("navCatalog: " + navCatalog.getAttribute("class"));
+		Assert.assertTrue(navCatalog.getAttribute("class").contains("menu-open"));
+//		Assert.assertEquals(navCatalog.getCssValue("color"), "rgb(255, 255, 255)");
+//		System.out.println(navCatalog.getAttribute("class"));
+//		Assert.assertTrue(navCatalog.getAttribute("class").contains("menu-open"));
+
+		WebElement navProducts = wait
+				.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='/Admin/Product/List']")));
+		navProducts.click();
+//		Assert.assertTrue(navCatalog.getAttribute("class").contains("active"));
+
+		checkTitleAndURL(AppConstants.PRODUCTS_URL, AppConstants.PRODUCTS_TITLE);
+		System.out.println("Title: " + driver.getTitle());
+
+//		Assert.assertTrue(driver.getCurrentUrl().contains("/Product/List"));
+//		WebElement productsPageTitle = driver.findElement(By.className("float-left"));
+//		Assert.assertTrue(productsPageTitle.getText().contains("Products"));
+
+	}
+
+//	public static void assertNavItemSelected(WebElement item) {
+//		Assert.assertTrue(item.getAttribute("class").contains("active"));
+//		Assert.assertEquals(item.getCssValue("color"), "rgb(255, 255, 255, 1)");
+//	}
+
+	public static void checkTitleAndURL(String url, String title) {
+		Assert.assertTrue(driver.getCurrentUrl().contains(url));
+		Assert.assertTrue(driver.getTitle().contains(title));
+
+		WebElement titleElement = driver.findElement(By.className("float-left"));
+		Assert.assertTrue(titleElement.getText().contains(title));
 	}
 }
