3b1d68f2bdedb5274bc8699297145037b52e185d
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2021-12-29 10:08:35 +0200

    Update MainTest.java
    Testing Login Page:
    - check page url + title
    - check email value
    - check password value
    - click login button

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import utils.AppConstants;
 import utils.DriversHandler;
@@ -13,6 +16,15 @@ public class MainTest {
 
 		driver = DriversHandler.initGoogleChromeDriver();
 		driver.get(AppConstants.BASE_URL);
-	
+
+		// ------------------- Login Page -------------------
+		Assert.assertTrue(driver.getCurrentUrl().contains(AppConstants.LOGIN_URL));
+		Assert.assertTrue(driver.getTitle().contains(AppConstants.LOGIN_PAGE));
+		WebElement loginEmail = driver.findElement(By.id("Email"));
+		WebElement loginPassword = driver.findElement(By.id("Password"));
+		WebElement loginBtn = driver.findElement(By.className("login-button"));
+		Assert.assertEquals(loginEmail.getAttribute("value"), "admin@yourstore.com");
+		Assert.assertEquals(loginPassword.getAttribute("value"), "admin");
+		loginBtn.click();
 	}
 }
